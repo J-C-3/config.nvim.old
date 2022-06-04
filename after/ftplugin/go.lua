fed5eef6d5b585723df2c1bf4be15e981e944c1f
distek <distek@protonmail.com>
2022-06-04 09:49:49 -0700

    Switch to lua for autocommands for go

---
+++ after/ftplugin/go.lua
@@ -0,0 +1,22 @@
+vim.api.nvim_create_augroup("Go", { clear = true })
+
+local opts = { noremap = true, silent = true }
+local map = function(mode, keys, command)
+    vim.api.nvim_set_keymap(mode, keys, command, opts)
+end
+
+vim.api.nvim_create_autocmd("BufEnter", {
+    pattern = { "go" },
+    callback = function()
+        vim.o.buflisted = false
+
+        map("n", "<leader>mk", ":mksession!")
+        map("n", "<leader>Gfs", ":GoFillStruct<cr>")
+        map("n", "<leader>Gie", ":GoIfErr<cr>")
+        map("n", "<leader>Gr", " :GoRun")
+        map("n", "<leader>Gt", " :GoTest")
+        map("n", "<leader>Gat", ":GoAddTest<cr>")
+    end,
+    group = "Go",
+})
+
