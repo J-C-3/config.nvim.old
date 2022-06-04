3e99da7504cc76e885221ceb837108d1114e2ecc
distek <distek@protonmail.com>
2022-06-03 21:05:43 -0700

    telescope command pallete

--- init.lua
+++ init.lua
@@ -392,6 +392,8 @@ require('packer').startup(function()
 
     -- Misc/QOL {{{
     use 'powerman/vim-plugin-AnsiEsc'
+    use { "LinArcX/telescope-command-palette.nvim" }
+
     use 'folke/which-key.nvim'
 
     use({
@@ -966,6 +968,67 @@ require('nvim-dap-virtual-text').setup()
 --         search = true, -- Requires hlslens to be loaded
 --     },
 -- })
+-- }}}
+
+-- Telescope {{{
+require('telescope').setup({
+    extensions = {
+        command_palette = {
+            { "File",
+                { "entire selection (C-a)", ':call feedkeys("GVgg")' },
+                { "save current file (C-s)", ':w' },
+                { "save all files (C-A-s)", ':wa' },
+                { "quit (C-q)", ':qa' },
+                { "file browser (C-i)", ":lua require'telescope'.extensions.file_browser.file_browser()", 1 },
+                { "search word (A-w)", ":lua require('telescope.builtin').live_grep()", 1 },
+                { "git files (A-f)", ":lua require('telescope.builtin').git_files()", 1 },
+                { "files (C-f)", ":lua require('telescope.builtin').find_files()", 1 },
+            },
+            { "Help",
+                { "tips", ":help tips" },
+                { "cheatsheet", ":help index" },
+                { "tutorial", ":help tutor" },
+                { "summary", ":help summary" },
+                { "quick reference", ":help quickref" },
+                { "search help(F1)", ":lua require('telescope.builtin').help_tags()", 1 },
+            },
+            { "Vim",
+                { "reload vimrc", ":source $MYVIMRC" },
+                { 'check health', ":checkhealth" },
+                { "jumps (Alt-j)", ":lua require('telescope.builtin').jumplist()" },
+                { "commands", ":lua require('telescope.builtin').commands()" },
+                { "command history", ":lua require('telescope.builtin').command_history()" },
+                { "registers (A-e)", ":lua require('telescope.builtin').registers()" },
+                { "colorshceme", ":lua require('telescope.builtin').colorscheme()", 1 },
+                { "vim options", ":lua require('telescope.builtin').vim_options()" },
+                { "keymaps", ":lua require('telescope.builtin').keymaps()" },
+                { "buffers", ":Telescope buffers" },
+                { "search history (C-h)", ":lua require('telescope.builtin').search_history()" },
+                { "paste mode", ':set paste!' },
+                { 'cursor line', ':set cursorline!' },
+                { 'cursor column', ':set cursorcolumn!' },
+                { "spell checker", ':set spell!' },
+                { "relative number", ':set relativenumber!' },
+                { "search highlighting (F12)", ':set hlsearch!' },
+            },
+            { "Debug",
+                {"Toggle breakpoint <leader>db", ":lua require('dap').toggle_breakpoint()"},
+                {"Step into <leader>di", ":lua require('dap').step_into()"},
+                {"Step over <leader>dn", ":lua require('dap').step_over()"},
+                {"Step out <leader>do", ":lua require('dap').step_out()"},
+                {"Up <leader>du", ":lua require('dap').up()"},
+                {"Down <leader>dd", ":lua require('dap').down()"},
+                {"Run to cursor <leader>drn", ":lua require('dap').run_to_cursor()"},
+                {"Continue <leader>dc", ":lua require('dap').continue()"},
+                {"DAP Open <leader>ds", ":lua require('dapui').open()"},
+                {"DAP Stop/Close <leader>dS", ":lua Util.dapStop()"},
+            }
+        }
+    }
+})
+
+require('telescope').load_extension('command_palette')
+
 -- }}}
 
 -- nvim-tree{{{
