240d3d61faac60cf91025a825b8ebcdbd3818fe1
J Christianson <j_christiansen@apple.com>
2022-09-20 10:16:39 -0500

    remove unnecessary plugins and adjust mappings

--- lua/user.d/potato-mappings.lua
+++ lua/user.d/potato-mappings.lua
@@ -18,8 +18,9 @@ map("t", "<C-p>", "<C-\\><C-n>")
 
 --- Buffer navigation
 --- :lua Util.skipQF("prev")<cr>
-map("n", "<leader>bd", ":BDelete this<CR>")
-map("n", "<leader>tb", "<cmd>lua require('telescope.builtin').buffers()<cr>")
+-- map("n", "<leader>bd", ":BDelete this<CR>")
+map("n", "<leader>bd", ":bp<bar>sp<bar>bn<bar>bd<cr>")
+map("n", "<leader>pb", "<cmd>lua require('telescope.builtin').buffers()<cr>")
 
 -- Navigate between panes easily
 map("n", "<leader>h", ":wincmd h<cr>")
@@ -31,20 +32,12 @@ map("n", "<leader>l", ":wincmd l<cr>")
 map("n", "<leader>+", ":vertical resize +5<CR>")
 map("n", "<leader>-", ":vertical resize -5<CR>")
 
--- TODO: Translate current ripgrep/fzf stuff to telecope
---
--- " RipGrep
--- nnoremap <leader>ps :Rg<SPACE>
-
--- " RipGrep fuzzy find files
--- nnoremap <leader>pf :Files<cr>
--- nnoremap <leader>pv :wincmd v <bar> :Files <cr>
--- nnoremap <leader>ph :wincmd s <bar> :Files <cr>
--- nmap <leader>y :History:<cr>
-map("n", "<leader>tf", "<cmd>lua require('telescope.builtin').find_files()<cr>")
-map("n", "<leader>tg", "<cmd>lua require('telescope.builtin').live_grep()<cr>")
-map("n", "<leader>tb", "<cmd>lua require('telescope.builtin').buffers()<cr>")
-map("n", "<leader>th", "<cmd>lua require('telescope.builtin').help_tags()<cr>")
+-- Telescope
+map("n", "<leader>pf", "<cmd>lua require('telescope.builtin').find_files()<cr>")
+map("n", "<leader>pv", ":wincmd v <bar> lua require('telescope.builtin').find_files()<cr>")
+map("n", "<leader>ph", ":wincmd s <bar> lua require('telescope.builtin').find_files()<cr>")
+map("n", "<leader>pg", "<cmd>lua require('telescope.builtin').live_grep()<cr>")
+map("n", "<leader>ph", "<cmd>lua require('telescope.builtin').help_tags()<cr>")
 
 -- }}}
 -- Git {{{
